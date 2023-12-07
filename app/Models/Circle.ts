274ad56fdfc9b341e4039927569eb2d4f2ff7842
utopico (linux) <sasha.petra96@gmail.com>
2023-12-07 23:34:45 +0100

    sm fix

--- app/Models/Circle.ts
+++ app/Models/Circle.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, beforeCreate, belongsTo, BelongsTo, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeCreate, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
 import { nanoid } from 'nanoid'
 
 import { string } from '@ioc:Adonis/Core/Helpers'
@@ -8,42 +8,42 @@ import Expense from './Expense'
 
 export default class Circle extends BaseModel {
 
-    @column({ isPrimary: true })
-    public id: number
+  @column({ isPrimary: true })
+  public id: number
 
-    @column()
-    public name: string
+  @column()
+  public name: string
 
-    @column()
-    public slug: string
+  @column()
+  public slug: string
 
-    @column()
-    public description: string
+  @column()
+  public description: string
 
-    @beforeCreate()
-    public static async onCreate(circle: Circle) {
-        console.log(circle)
-        if (circle.$dirty.name) {
-            circle.slug = string.dashCase(string.noCase(circle.name + " " + nanoid(4)))
-        }
+  @beforeCreate()
+  public static async onCreate(circle: Circle) {
+    console.log(circle)
+    if (circle.$dirty.name) {
+      circle.slug = string.dashCase(string.noCase(circle.name + " " + nanoid(4)))
     }
+  }
 
-    @manyToMany(() => User, {
-        pivotTable: "circles_users",
-        pivotTimestamps: true
-    })
-    public users: ManyToMany<typeof User>
+  @manyToMany(() => User, {
+    pivotTable: "circles_users",
+    pivotTimestamps: true
+  })
+  public users: ManyToMany<typeof User>
 
-    @manyToMany(() => Expense, {
-        pivotTable: "circles_expenses",
-        pivotTimestamps: true
-    })
-    public expenses: ManyToMany<typeof Expense>
+  @manyToMany(() => Expense, {
+    pivotTable: "circles_expenses",
+    pivotTimestamps: true
+  })
+  public expenses: ManyToMany<typeof Expense>
 
-    @column.dateTime({ autoCreate: true })
-    public createdAt: DateTime
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
 
-    @column.dateTime({ autoCreate: true, autoUpdate: true })
-    public updatedAt: DateTime
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
 
 }
